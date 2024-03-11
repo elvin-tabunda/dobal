607894c138cfd9a69df3ae7476694c7c18af6a1f
Elvin <tabundaelvin@gmail.com>
2024-03-11 10:16:38 +0800

    Update email.js

--- js/email.js
+++ js/email.js
@@ -8,9 +8,6 @@ function sendEmail(){
       userNumCorner : document.getElementById('userNumCorner').value,
       userPurpose : document.getElementById('userPurpose').value
     }
-    emailjs.init({
-        publicKey: 't1NJjDd5QqzKeBUjK',
-    });
     
     emailjs.send('service_m4g4lpv', 'template_y5a041h', params).then(function() {
       alert('Request Form Sent!');
